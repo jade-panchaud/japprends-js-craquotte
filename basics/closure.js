c2498aa3aeba75c345a9ef440a278c98e4b24ac3
jade-panchaud <jade.panchaud@gmail.com>
2021-05-28 19:37:23 +0200

    learning closure

--- basics/closure.js
+++ basics/closure.js
@@ -1,2 +1,30 @@
 //Global variables can be private with closures
+//In a web page, global variables = window object.
 
+//Isn't working, don't inc because can't access parent scope
+function addNotWorking(){
+    let count = 0;
+
+    return function (){
+        count++;
+        return count;
+    }
+}
+
+//Use a self-invoking function
+//Works because access the count in the parent
+//scope (self-invoking)
+//Closure = function having access to the parent scope,
+//even if the parent function has closed.
+const add = (function() {
+    let count = 0;
+
+    return function () {
+        count++;
+        return count;
+    }
+})();
+
+console.log(add());
+console.log(add());
+console.log(add());
